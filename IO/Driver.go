29f5017373b94b068e2bc69c0267105f03535ce5
stian <s.3_dokken@hotmail.com>
2015-03-03 17:12:00 +0100

    Skeleton to driver

--- IO/Driver.go
+++ IO/Driver.go
@@ -1,20 +1,57 @@
-
-
-package main
+package Driver
 
 /*
 #cgo LDFLAGS: -lcomedi -lm
-#include "io.h"
+#include "elev.h"
 */
-
 import "C"
 
-func set_button_lamp(button int, floor int, value int){
-	if (value) > 0 {
-		C.io_set_bit(lamp_channel_matrix[floor][button])
+
+//Define all neassacary enums
+
+func InitializeDriver(){
+	//start thread pollUserOrders
+	//start thread pollFLoorSensors
+	//start thread driverHandler
+}
+
+
+func driverHandler(){
+	for{
+		select{
+			case order 	 := chUserOrder:
+				//format order
+				//send order
+			case newFloor:= chArraivedFloor:
+				//format newFloor
+				//send newfloor 
+			case commandfromControl := chCommandFromControl:
+				//do what the command says
+				//define a protocol when we implement the control module
+		}
 	}
 }
 
-func main(){
-	set_button_lamp(0,1,1)
+
+func pollUserOrders(chUserOrder chan){
+//Loops over all order getfunctions in the C kode
+//Only button orders
 }
+
+
+func pollFLoorSensors(ch chanFloorSensor){
+//Loops through all floor sensors and reads them
+
+}
+
+func formatOrder(order){
+	//Format the order
+}
+
+func formatNewFloor(newFloor){
+	//Format the newFLoor
+}
+
+
+
+
