ce639dc8548e70f1612c8719662127ef6158e070
stian <stian>
2015-03-13 14:18:52 +0100

    Added source features

--- io/io.go
+++ io/io.go
@@ -1,34 +1,38 @@
 package io
 
 import "Heisprosjekt/driver"
+import "Heisprosjekt/source"
 import "runtime"
-import"fmt"
 
-var N_FLOOR int
-
-//cmd is shorten with command 
-//TODO: Fix this struct to be more generical when it comes to commands
-type Cmd struct{
-	cmdType int
-	floor int
-	value int
-	buttonType int
+//Command is shorten with command 
+type Commmad struct{
+	commandType int
+	floor 		int
+	value 		int
+	buttonType 	int
 }
 
-//TODO implement a order type whitch is formated by the formate functions
-		
 const(
 	SET_BUTTON_LAMP				= 0
 	SET_MOTOR_DIR				= 1
 	SET_FLOOR_INDICATOR_LAMP	= 2
 )
 
-func InitializeIo(chCmdCtrl chan Cmd, chCtrl chan int){
-	
+const(
+	OFF			=  0
+	ON 			=  1
+)
+
+
+var n_floor
+
+func InitializeIo(chCommand chan Command, chButtonOrderToControl chan source.ButtonOrder){
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	N_FLOOR 		= driver.GetN_FLOOR()
-	chButtonPressed := make(chan []int)
+	n_floor = source.GetNFloors()
+
+	chButtonOrder := make(chan source.BtnOrder)
 	chFloorSensor  	:= make(chan int)
 	
 	if err:=driver.Init(); err<0{
@@ -36,53 +40,52 @@ func InitializeIo(chCmdCtrl chan Cmd, chCtrl chan int){
 		//TODO Handle error, exit. Try one more time
 	}
 
-	go ioHandler(chCmdCtrl,chCtrl,chButtonPressed,chFloorSensor)
+	go ioHandler(chCommand,chButtonOrderToControl,chButtonOrder,chFloorSensor)
 
-	go pollButtonOrders(chButtonPressed)
+	go pollButtonOrders(chButtonOrder)
 
-	go pollFLoorSensors(chFloorSensor)
+	go pollFloorSensors(chFloorSensor)
 }
 
-func ioHandler(chCmdCtrl chan Cmd, chCtrl chan int, chButtonPressed chan []int, chFloorSensor chan int){
+func ioHandler(chCommand chan Command, chButtonOrderToControl chan source.ButtonOrder, chFloorSensor,
+				chButtonOrder chan source.ButtonOrder, chFloorSensor chan int){
 	for{
 		select{
-			case <- chButtonPressed:
-				//format order
+			case order:=<- chButtonOrder:
 				//send order
-				chCtrl <- 1
+				chButtonOrderToControl <- 1
 
-			case <- chFloorSensor:
-				//format newFloor
+			case floor:= <- chFloorSensor:
 				//send newfloor
-				chCtrl <- 2
+				chButtonOrderToControl <- 2
 
-			case cmdCtrl := <- chCmdCtrl:
-				doCmd(cmdCtrl)
+			case command := <- chCommand:
+				doCommand(command)
 		}
 	}
 }
 
-func pollButtonOrders(chButtonPressed chan []int){
+func pollButtonOrders(chButtonOrder chan source.ButtonOrder){
 	//TODO: Test funksjonen og fiks problemet med slices!!!
 	for{
-		for floor := 0; floor < N_FLOOR; floor++ {
+		for floor := 0; floor < n_floor; floor++ {
 			
 			if(floor > 0 && driver.GetButtonSignal(driver.BUTTON_CALL_UP,floor)==1){
-				chButtonPressed <- []int{0,floor}
+				chButtonOrder <- []int{0,floor}
 			}
 			
-			if(floor < N_FLOOR-2 && driver.GetButtonSignal(driver.BUTTON_CALL_DOWN,floor)==1){
-				chButtonPressed <- []int{1,floor}
+			if(floor < n_floor-2 && driver.GetButtonSignal(driver.BUTTON_CALL_DOWN,floor)==1){
+				chButtonOrder <- []int{1,floor}
 			}
 			
 			if(driver.GetButtonSignal(driver.BUTTON_COMMAND,floor)==1){
-				chButtonPressed <- []int{2,floor}
+				chButtonOrder <- []int{2,floor}
 			}
 		}
 	}
 }
 
-func pollFLoorSensors(chFloorSensor chan int){
+func pollFloorSensors(chFloorSensor chan int){
 	//TODO: Test funksjonen
 	for{	
 		if floor := driver.GetFloorSensor(); floor != -1{
@@ -91,32 +94,20 @@ func pollFLoorSensors(chFloorSensor chan int){
 	}
 }
 
-func doCmd(cmdCtrl Cmd){
+func doCommand(command Command){
 	//TODO  Fiks enum problemet med typer.
-	if(SET_BUTTON_LAMP == cmdCtrl.cmdType){
-		driver.SetButtonLamp(1, cmdCtrl.floor, cmdCtrl.value)
+
+	if(SET_BUTTON_LAMP == command.CommandType){
+		driver.SetButtonLamp(1, CommandControl.floor, CommandControl.value)
 	}
 
-	if(SET_MOTOR_DIR == cmdCtrl.cmdType){
+	if(SET_MOTOR_DIR == command.CommandType){
 		driver.SetMotorDir(1)
 	}
 
-	if(SET_FLOOR_INDICATOR_LAMP == cmdCtrl.cmdType){
+	if(SET_FLOOR_INDICATOR_LAMP == command.CommandType){
 		driver.SetFloorIndicatorLamp(2)
 	}
 	//implement SetDoorOpenLamp
 }
-
-func formatOrder(order int) int{
-	//Format the order
-	return order
-}
-
-func formatNewFloor(newFloor int) int{
-	//Format the newFLoor
-	return newFloor
-}
-
-
-
 
