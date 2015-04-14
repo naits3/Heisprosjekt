846b66b91d9881c43002340fb19a0dbc96036d6d
stian <s.3_dokken@hotmail.com>
2015-04-14 10:13:21 +0200

    Controller ready for queue and fixed same floor order bug

--- controller/controller.go
+++ controller/controller.go
@@ -16,6 +16,7 @@ const (
 
 var state int 
 var nextFloor int
+var orderFinished bool
 var currentFloor int
 
 var chCommandFromControl 	= make(chan src.Command)
@@ -26,23 +27,25 @@ var chFloorSensorToControl	= make(chan int)
 var chNewFloor				= make(chan int)
 var chNewOrder 				= make(chan src.ButtonOrder)
 var chNewDirection			= make(chan int)
-var chOrderIsFinished 		= make(chan src.ButtonOrder)
+var chOrderIsFinished 		= make(chan bool)
 var chNewOrdersFromQueue 	= make(chan src.ElevatorData)
 var chNewNextFloorFromQueue = make(chan int)
 
 func InitController() {
 	
+	direction := 0 
+
 	io.InitIo(chCommandFromControl, chButtonOrderToControl, chFloorSensorToControl)
 	
 	goDownUntilReachFloor()	
-	state = IDLE
+	
 
 	queue.InitQueue(channels here...) // Let Queue init network
 	
 	go controllerHandler()
 }
 
-func controllerHandler(){
+func controllerHandler(direction int){
 	for {
 		select {
 			case ioOrder:= <- chButtonOrderToControl:
@@ -52,23 +55,28 @@ func controllerHandler(){
 				switch state{
 					case IDLE:
 						if currentFloor != nextFloor{
-							dir := findElevatorDirection()
-							chCommandFromControl <- src.Command{src.SET_MOTOR_DIR,dir,-1,src.BUTTON_NONE}
+							direction = findElevatorDirection()
+							chCommandFromControl <- src.Command{src.SET_MOTOR_DIR,direction,-1,src.BUTTON_NONE}
 							state = MOVING
 						}else{
 							chCommandFromControl <- src.Command{src.SET_MOTOR_DIR,src.DIR_STOP,-1,src.BUTTON_NONE}
 							state = IDLE
-							//Hvordan skal vi vite at det ikke er en inside ordere??
+							if(!orderFinished){
+								openDoor()
+								orderFinished = true
+								chOrderIsFinished <- true
+							}
 						}
 					case MOVING:
 						if currentFloor != nextFloor{
-							dir := findElevatorDirection()
-							chCommandFromControl <- src.Command{src.SET_MOTOR_DIR,dir,-1,src.BUTTON_NONE}
+							direction = findElevatorDirection()
+							chCommandFromControl <- src.Command{src.SET_MOTOR_DIR,direction,-1,src.BUTTON_NONE}
 							state = MOVING
 						}else{
 							chCommandFromControl <- src.Command{src.SET_MOTOR_DIR,src.DIR_STOP,-1,src.BUTTON_NONE}
-							state = DOOR_OPEN
-							go openDoor()
+							openDoor()
+							orderFinished = true
+							chOrderIsFinished <- true
 						}
 					case DOOR_OPEN:
 						continue
@@ -80,37 +88,33 @@ func controllerHandler(){
 		
 
 			case nextFloor = <-chNewNextFloorFromQueue:
-				continue
+				orderFinished = false
 		}				
 	}
 }
 
 func setLights(knowOrders src.ElevatorData){
-	//println("Enter setLights!")
-	tools.PrintQueue(knowOrders) // DELETE THIS!!
 	for floor := 0; floor < src.N_FLOORS; floor ++ {
-		//println(floor)
 		println(knowOrders.OutsideOrders[floor][src.BUTTON_UP])
 
 		chCommandFromControl <- src.Command{src.SET_BUTTON_LAMP,knowOrders.OutsideOrders[floor][src.BUTTON_UP],     floor,src.BUTTON_UP}
 		chCommandFromControl <- src.Command{src.SET_BUTTON_LAMP,knowOrders.OutsideOrders[floor][src.BUTTON_DOWN],   floor,src.BUTTON_DOWN}
 		chCommandFromControl <- src.Command{src.SET_BUTTON_LAMP,knowOrders.InsideOrders[floor], 					floor,src.BUTTON_INSIDE}
 	}
-	//println("Goodbye, end of setLights!")
 }
 
 func goDownUntilReachFloor(){
 	chCommandFromControl <- src.Command{src.SET_MOTOR_DIR,src.DIR_DOWN,-1,src.BUTTON_NONE}
-	
 	currentFloor	= <- chFloorSensorToControl
 	nextFloor 		= currentFloor
-	
 	chCommandFromControl <- src.Command{src.SET_MOTOR_DIR,src.DIR_STOP,-1,src.BUTTON_NONE}
 	chCommandFromControl <- src.Command{src.SET_FLOOR_INDICATOR_LAMP,src.ON,currentFloor,src.BUTTON_NONE}
+	state = IDLE
 }
 
 
 func openDoor(){
+    state = DOOR_OPEN
     command1 := src.Command{src.SET_DOOR_OPEN_LAMP,src.ON,-1,src.BUTTON_NONE}
 	command2 := src.Command{src.SET_DOOR_OPEN_LAMP,src.OFF,-1,src.BUTTON_NONE}
 	chCommandFromControl <- command1
@@ -126,3 +130,9 @@ func findElevatorDirection() int {
 		return src.DIR_DOWN
 	}
 }
+
+/*
+BUGS
+If a ordere for next floor is recived and the next floor equals the current floor, the confirmation wont be sent.
+
+*/
