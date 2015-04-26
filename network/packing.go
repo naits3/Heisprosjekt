1c318e00612596e96737cfd4ddd1e2a0977f99ca
martiskj <martiskj@stud.ntnu.no>
2015-04-26 11:17:05 +0200

    json is now working properly. Removed the unused packing functions

--- network/packing.go
+++ network/packing.go
@@ -2,31 +2,7 @@ package network
 
 import (
 	"encoding/json"
-	"Heisprosjekt/src"
 )
-
-
-func PackElevatorData(unpackedElevatorData src.ElevatorData) []byte {
-	packedElevatorData,_ := json.Marshal(unpackedElevatorData)
-	return packedElevatorData
-}
-
-func UnpackElevatorData(packedElevatorData []byte) src.ElevatorData {
-	var unpackedElevatorData src.ElevatorData
-	json.Unmarshal(packedElevatorData, &unpackedElevatorData)
-	return unpackedElevatorData
-}
-
-func PackOrder(unpackedOrder src.ButtonOrder) []byte {
-	packedOrder, _ := json.Marshal(unpackedOrder)
-	return packedOrder
-}
-
-func UnpackOrder(packedOrder []byte) src.ButtonOrder {
-	var unpackedOrder src.ButtonOrder
-	json.Unmarshal(packedOrder, &unpackedOrder)
-	return unpackedOrder
-}
 
 func PackMessage(unpackedMessage Message) []byte {
 	packedMessage, _ := json.Marshal(unpackedMessage)
